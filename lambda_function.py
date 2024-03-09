3158915ae568ae3aa06f5ee65f7f6ed83bde83d2
unknown <rhishikesh.padole@gamil.com>
2024-03-09 11:09:28 +0530

    Initial Commit

---
+++ lambda_function.py
@@ -0,0 +1,10 @@
+import json
+import requests
+import pandas
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
